b6ec904db9782a1149807ee79e58b7e0dead4a61
SABUJ MAITY <34521697+SabujPro@users.noreply.github.com>
2020-04-10 14:05:49 +0530

    Update Tv Shows Popularity Analysis.py

--- Tv Shows Popularity Analysis.py
+++ Tv Shows Popularity Analysis.py
@@ -31,3 +31,45 @@ def webscraping():
         print()
     else:
         print("List is empty/account name not found.")
+        
+        from importlib import reload
+import sys
+from imp import reload
+import warnings
+warnings.filterwarnings('ignore')
+if sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+import pandas as pd
+
+df1 = pd.read_csv('labeledTrainData.tsv', delimiter="\t")
+df1 = df1.drop(['id'], axis=1)
+df1.head()
+df2 = pd.read_csv('imdb_master.csv',encoding="latin-1")
+df2.head()
+df2 = df2.drop(['Unnamed: 0','type','file'],axis=1)
+df2.columns = ["review","sentiment"]
+df2.head()
+df2 = df2[df2.sentiment != 'unsup']
+df2['sentiment'] = df2['sentiment'].map({'pos': 1, 'neg': 0})
+df2.head()
+df = pd.concat([df1, df2]).reset_index(drop=True)
+df.head()
+import re
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words("english")) 
+lemmatizer = WordNetLemmatizer()
+
+
+def clean_text(text):
+    text = re.sub(r'[^\w\s]','',text, re.UNICODE)
+    text = text.lower()
+    text = [lemmatizer.lemmatize(token) for token in text.split(" ")]
+    text = [lemmatizer.lemmatize(token, "v") for token in text]
+    text = [word for word in text if not word in stop_words]
+    text = " ".join(text)
+    return text
+
+df['Processed_Reviews'] = df.review.apply(lambda x: clean_text(x))
